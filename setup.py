e7f1c3d15c52512589312564c0179f864ceb96e8
raybittuiq <bittu.kumar@squadstack.com>
2024-12-11 13:48:32 +0530

    fixed monaco

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(
 
 setup(
     name="squad-django-monaco",
-    version="1.2.2",
+    version="1.2.5",
     packages=["monaco"],
     description="Monaco editor widgets in the Django Admin",
     include_package_data=True,
@@ -20,6 +20,6 @@ setup(
     platforms=["OS Independent"],
     license="MIT",
     install_requires=[
-        "Django<=4.0",
+        "Django<=5.2",
     ],
 )
